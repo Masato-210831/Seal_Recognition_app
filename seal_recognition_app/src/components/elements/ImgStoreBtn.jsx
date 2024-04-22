7bd25cd603965e34a6fa549608058d5b98b1d7b8
masato-210831 <b86500b@gmail.com>
2024-04-22 20:29:18 +0900

    modified dockerfile & ImgStoreBtn for poetry virtual problem

--- seal_recognition_app/src/components/elements/ImgStoreBtn.jsx
+++ seal_recognition_app/src/components/elements/ImgStoreBtn.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DetectionBtn from "./DetectionBtn";
-import SendIcon from '@mui/icons-material/Send';
-import LoadingButton from '@mui/lab/LoadingButton';
+import SendIcon from "@mui/icons-material/Send";
+import LoadingButton from "@mui/lab/LoadingButton";
 
-
-const ImgStoreBtn = ({inputFiles, setStoredResult, setErrorMessage}) => {
-
-  const [initState, setInitState] = useState(false)
+const ImgStoreBtn = ({ inputFiles, setStoredResult, setErrorMessage }) => {
+  const [initState, setInitState] = useState(false);
 
   const files = inputFiles; // 送信する画像配列
 
@@ -19,7 +17,6 @@ const ImgStoreBtn = ({inputFiles, setStoredResult, setErrorMessage}) => {
 
   // ボタンが押された時の挙動
   const pressbtn = () => {
-
     // アップロードファイルが空の場合
     if (files.length === 0) {
       setErrorMessage("ファイルをアップロードしてください");
@@ -28,12 +25,14 @@ const ImgStoreBtn = ({inputFiles, setStoredResult, setErrorMessage}) => {
       setErrorMessage("");
     }
 
-    setStoredResult("")
-    setInitState(true) // 送信中表示
+    setStoredResult("");
+    setInitState(true); // 送信中表示
 
     // POSTするURL
     // const post_url = "http://127.0.0.1:8000/uploadfile/ ";
-    const post_url = "https://detection-image-vdaepgddza-uc.a.run.app/uploadfile/ ";
+    // const post_url = "https://test-vdaepgddza-uc.a.run.app/uploadfile/ ";
+    const post_url =
+      "https://detection-image-vdaepgddza-uc.a.run.app/uploadfile/ ";
 
     // FormDataオブジェクトに追加
     const formData = new FormData();
@@ -48,11 +47,11 @@ const ImgStoreBtn = ({inputFiles, setStoredResult, setErrorMessage}) => {
       })
       .then(function (response) {
         setStoredResult(response);
-        setInitState(false) // 送信中非表示
+        setInitState(false); // 送信中非表示
       })
       .catch(function (error) {
         console.log(error);
-        setInitState(false) // 送信中非表示
+        setInitState(false); // 送信中非表示
         if (error.response) {
           setErrorMessage(error.response.data.detail); // エラーメッセージを設定
         } else {
@@ -64,15 +63,15 @@ const ImgStoreBtn = ({inputFiles, setStoredResult, setErrorMessage}) => {
   return (
     <>
       <LoadingButton
-          onClick={pressbtn}
-          endIcon={<SendIcon />}
-          loading={initState}
-          loadingPosition="end"
-          variant="contained"
-          sx={{width:170, fontSize:{sm:16, xs:12}}}
-        >
-          <span>ファイルの送信</span>
-        </LoadingButton>
+        onClick={pressbtn}
+        endIcon={<SendIcon />}
+        loading={initState}
+        loadingPosition="end"
+        variant="contained"
+        sx={{ width: 170, fontSize: { sm: 16, xs: 12 } }}
+      >
+        <span>ファイルの送信</span>
+      </LoadingButton>
     </>
   );
 };
